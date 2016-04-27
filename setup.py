e8039a6d744b92930a8c2e254fa5c60262d2b21a
Wouter Bolsterlee <wouter@bolsterl.ee>
2016-04-27 17:10:58 +0200

    Setup file and requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='haags',
+    version='0.0.1',
+    author="Wouter Bolsterlee",
+    author_email="wouter@bolsterl.ee",
+    install_requires=['attrs'],
+    py_modules=['haags'],
+    license="BSD",
+)
